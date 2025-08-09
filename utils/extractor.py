24133116b8685277c16b0c33971ae378a08578ee
Krishthick <79474339+KRISHTHICK@users.noreply.github.com>
2025-08-09 14:11:17 +0530

    Create extractor.py

---
+++ utils/extractor.py
@@ -0,0 +1,12 @@
+import fitz  # PyMuPDF
+import docx2txt
+
+def extract_text_from_pdf(file_path):
+    doc = fitz.open(file_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
+
+def extract_text_from_docx(file_path):
+    return docx2txt.process(file_path)
